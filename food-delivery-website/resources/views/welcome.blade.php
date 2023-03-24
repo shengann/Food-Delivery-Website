365c9353bc017cb7f52f72e152b5b675d81fde77
Chlorrie <jellyfish.346@gmail.com>
2023-03-25 00:06:29 +0800

    test yee ching add to web.php

--- food-delivery-website/resources/views/welcome.blade.php
+++ food-delivery-website/resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
         <li><a class="dropdown-item" href="#">Action</a></li>
         <li><a class="dropdown-item" href="#">Another action</a></li>
         <li><a class="dropdown-item" href="#">Something else here</a></li>
+        <h1>niama chao ji bai</h1>
     </ul>
 </div>
 
