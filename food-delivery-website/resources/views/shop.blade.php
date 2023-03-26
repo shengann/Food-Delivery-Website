ad2cbd89c8450d9ca3ae81aa835aaee36c9d1a99
ShuYiKang <shuyikang00s@gmail.com>
2023-03-27 03:54:44 +0800

    Added shop view

--- food-delivery-website/resources/views/shop.blade.php
+++ food-delivery-website/resources/views/shop.blade.php
@@ -1,7 +1,8 @@
-<h1>Shop name</h1>
+<h1>{{$products[0]['id']}}</h1>
 
+<table>
 @foreach ($products as $product)
-{
-    <td>{{$product}}</td>
-}
+    <tr>{{$product['product_name']}}</tr><br>
+    <tr>{{$product['product_price']}}</tr><br>
 @endforeach
+</table>
