2e8b13905ce70e4828b2cf459b15cb50008c87c7
ShuYiKang <shuyikang00s@gmail.com>
2023-03-26 15:22:44 +0800

    Add getProducts function

--- food-delivery-website/app/Http/Controllers/ProductController.php
+++ food-delivery-website/app/Http/Controllers/ProductController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Products;
 use Illuminate\Http\Request;
+Use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
-    //
+    public function getProducts(){
+        $data = Products::where('shop_id', '1')->get();
+        return view('shop', ['products' => $data]);
+    }
 }
