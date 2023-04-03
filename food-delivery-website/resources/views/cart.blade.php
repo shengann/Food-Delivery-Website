67d63788ec72269844e834414ab1d1e7874acc80
ShuYiKang <shuyikang00s@gmail.com>
2023-04-04 00:35:53 +0800

    Added link back to shop and quantity of product

--- food-delivery-website/resources/views/cart.blade.php
+++ food-delivery-website/resources/views/cart.blade.php
@@ -1,11 +1,12 @@
 <x-header />
 <div class="container-fluid" style="background-color:#F1F1F1">
-
+<a href="/shop/1">Back to Shop</a>
 <table>
 @foreach (session('cart') as $id => $item)
     <tr>
         <td>{{$item['name']}}</td>
         <td>{{$item['price']}}</td>
+        <td>{{$item['quantity']}}</td>
     </tr>
 @endforeach
 </table>
