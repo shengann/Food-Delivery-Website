225da52afce7a4330020ae421758aa975373e441
ShuYiKang <shuyikang00s@gmail.com>
2023-04-05 00:57:03 +0800

    Modify ui of header

--- food-delivery-website/resources/views/components/header.blade.php
+++ food-delivery-website/resources/views/components/header.blade.php
@@ -1,13 +1,13 @@
-<div class="container-fluid text-left"  >
+<div class="container-fluid position-relative">
   <div class="row">
     <div class="col">
     <img src="{{ asset('img/logo.png') }}" alt="" style="width: 50px;">
     </div>
-    <div class="col-11">
+    <div class="col">
     <h2>foodTiger</h2>
     </div>
-    <div class="col">
-    <a href="/showCart">Cart</a>
+    <div class="col ">
+    <a class="btn btn-success position-absolute top-50 start-90 translate-middle" href="/showCart">Cart</a>
     </div>
   </div>
 </div>
