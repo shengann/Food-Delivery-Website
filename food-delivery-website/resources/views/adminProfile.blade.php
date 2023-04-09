e4f2b53a21014fa6db9a76941966fab2c45c2ab7
shengann <shengann2412@gmail.com>
2023-04-09 10:14:52 +0800

    rearrange the code for admin

--- food-delivery-website/resources/views/adminProfile.blade.php
+++ food-delivery-website/resources/views/adminProfile.blade.php
@@ -1,51 +1,5 @@
 <head>
     <link rel="stylesheet" href="/css/app.css">
-    @section('styles')
-    <style>
-        body {
-            background: #eee;
-        }
-
-        #side_nav {
-            background: #000;
-            min-width: 250px;
-            max-width: 250px;
-            height: 100%;
-            overflow-y: auto
-        }
-
-        .main-container {
-            display: flex;
-            height: 100vh;
-        }
-
-        .content {
-            min-height: 100vh;
-            width: 100%
-        }
-
-        hr.n-color {
-            background: #eee;
-        }
-
-        .sidebar li.active {
-            background: #eee;
-            border-radius: 8px;
-        }
-
-        .sidebar li.active a,
-        .sidebar li.active a:hover {
-            color: #000
-        }
-
-        .sidebar li a {
-            color: #fff
-        }
-    </style>
-
-    @endsection
-    @yield('styles')
-
 </head>
 
 <body>
@@ -64,15 +18,61 @@
         </div>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-        <script>
-            $(".sidebar ul li").on('click', function() {
-                $(".sidebar ul li.active").removeClass('active');
-                $(this).addClass('active');
-            });
-        </script>
-
         <div class="content">
 
         </div>
     </div>
+
+    <script>
+        $(".sidebar ul li").on('click', function() {
+            $(".sidebar ul li.active").removeClass('active');
+            $(this).addClass('active');
+        });
+    </script>
 </body>
+
+@section('styles')
+<style>
+    body {
+        background: #eee;
+    }
+
+    #side_nav {
+        background: #000;
+        min-width: 250px;
+        max-width: 250px;
+        height: 100%;
+        overflow-y: auto
+    }
+
+    .main-container {
+        display: flex;
+        height: 100vh;
+    }
+
+    .content {
+        min-height: 100vh;
+        width: 100%
+    }
+
+    hr.n-color {
+        background: #eee;
+    }
+
+    .sidebar li.active {
+        background: #eee;
+        border-radius: 8px;
+    }
+
+    .sidebar li.active a,
+    .sidebar li.active a:hover {
+        color: #000
+    }
+
+    .sidebar li a {
+        color: #fff
+    }
+</style>
+
+@endsection
+@yield('styles')
