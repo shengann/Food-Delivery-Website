c220ea8f7d45de341fc995a74d00790bd833c845
shengann <shengann2412@gmail.com>
2023-04-12 14:53:42 +0800

    added OrderDetails popup

--- food-delivery-website/resources/js/components/OrderDetails.js
+++ food-delivery-website/resources/js/components/OrderDetails.js
@@ -3,23 +3,54 @@ import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class OrderDetailsModal extends Component {
     state = {
-        orderDetails: null,
+        orderDetails: [],
     };
 
     componentDidMount() {
-        // Make API call to fetch order details based on this.props.id
-        // fetch(`/api/orders/${this.props.id}`)
-        //     .then(response => response.json())
-        //     .then(data => this.setState({ orderDetails: data }));
+      this.loadOrderItem()
+    }
+
+    loadOrderItem() {
+        const url = `/api/order-items/${this.props.orderId}`;
+        axios.get(url).then((response) => {
+            this.setState({
+                orderDetails: response.data
+            })
+        })
     }
 
     render() {
-        const { orderDetails } = this.state;
+        let orderDetails = this.state.orderDetails.map((orderDetail) => {
+            return (
+                <tr key={order.id}>
+                    <td className="text-center">{orderDetail.product_id}</td>
+                    <td className="text-center">{orderDetail.quantity}</td>
+                    <td className="text-center">
+                        RM {orderDetail.item_price}
+                    </td>
+                </tr>
+            )
+        })
 
         return (
             <Modal isOpen={this.props.isOpen} toggle={this.props.toggle}>
-                <ModalHeader toggle={this.props.toggle}>Order Details</ModalHeader>
+                <ModalHeader toggle={this.props.toggle}>Order Details </ModalHeader>
                 <ModalBody>
+                    <div className="mx-1 my-1">
+                        <table className="table table-striped table-bordered">
+                            <thead>
+                                <tr>
+                                    <th className="text-center" >Item</th>
+                                    <th className="text-center" >Quantity</th>
+                                    <th className="text-center" >Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {orderDetails}
+                            </tbody>
+
+                        </table>
+                    </div>
                 </ModalBody>
                 <ModalFooter>
                     <button onClick={this.props.toggle}>Close</button>
