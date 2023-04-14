41f40a8965ada477c72fb794d5e73e9a5b62b37e
ShuYiKang <shuyikang00s@gmail.com>
2023-04-14 15:58:45 +0800

    add seesion to cart

--- food-delivery-website/resources/views/cart.blade.php
+++ food-delivery-website/resources/views/cart.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.app')
 @section('content')
-
-@if(!session('shop'))
-<a class="btn btn-success position-relative" href="/home">Back to Home</a><br><br>
-@else
 <a class="btn btn-success position-relative" href="/shop/{{session('shop')}}}">Back to Shop</a><br><br>
-@endif
-
 <div class="container-fluid" style="background-color:#F1F1F1">
+
+
+
+<input type="hidden" id="session-data"x value="<?= htmlspecialchars(json_encode(session('cart'))) ?>">
 
 
 <div id='popup'></div>
@@ -16,13 +14,6 @@
 @if (!session('cart'))
 <img class="position-absolute top-50 start-50 translate-middle w-25 p-3"  src="{{ asset('img/icons/emptycart.png') }}" alt="">
 <p class="position-absolute bottom-20 start-50 translate-middle-x --bs-tertiary-color fs-3">Oops! There's nothing in your cart yet</p>
-@endif
-
-@if(session('cart'))
-<!--<div id='confirm'></div>-->
-<div style="width:100%">
-<a style="margin-left:70%;" class="btn btn-success position-relative" href="/confirmOrder">Confirm Order</a><br><br>
-</div>
 @endif
 
 @endsection
